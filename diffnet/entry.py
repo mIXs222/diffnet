75ce097aff40b6df736ba9778211baf714a88912
jwu2012 <jiaxinw3@illinois.edu>
2021-03-29 13:47:05 -0400

    Update entry.py

--- diffnet/entry.py
+++ diffnet/entry.py
@@ -19,7 +19,7 @@ def executeTrainModel(config_path, model_name):
     #print('System start to prepare parser config file...')
     conf = ParserConf(config_path)
     conf.parserConf()
-    print conf.topk
+    print(conf.topk)
     
     #print('System start to load TensorFlow graph...')
     model = eval(model_name)
