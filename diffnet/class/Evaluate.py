143bbe940820d496ad12ecb5376a51691822c81e
jwu2012 <jiaxinw3@illinois.edu>
2021-03-29 14:37:13 -0400

    Update Evaluate.py

--- diffnet/class/Evaluate.py
+++ diffnet/class/Evaluate.py
@@ -34,6 +34,9 @@ class Evaluate():
         index = 0
         for _ in range(num_procs):
             if index + batch_size < len(user_list):
+                print(len(user_list))
+                print(index)
+                print(batch_size)
                 batch_user_list = user_list[index:index+batch_size]
                 index = index + batch_size
             else:
